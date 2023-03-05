957244be59f3fb335554c09ad0869ce3cde8790d
hedysnika <arebanikko@gmail.com>
2023-03-04 20:17:42 -0800

    ageagaeg

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="flex justify-center items-center h-screen bg-black pt-10 h-full min-h-screen w-full">
+    
+      <div className=' text-white rounded text-white' type='text'> <input className='outline-none text-white bg-black border border-white px-2 p-1 mr-2 rounded' type="text" /> <button className='border border-white px-2 py-1 rounded hover:bg-blue-600'>search </button> </div>
+    </div>
+
+
+  )
+}
+
+export default App
